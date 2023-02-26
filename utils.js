82b164c1fac5dda068af830c78a62e6d53fd9b06
Dmitriy <krudda@yandex.ru>
2023-02-26 18:37:34 +0300

    feat - add common variables

--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
 export const DEFAULT_REGION = "eu-west-1";
 export const PRODUCT_TABLE_NAME= "Products";
 export const STOCKS_TABLE_NAME= "Stocks";
+export const PRODUCT_BUCKET_NAME= "aws-krudda-first-app-upload";
